935b05387ca04196014b6e0c80b0abc3fee64dec
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-10-17 08:51:22 -0300

    Create interface
    Co-Authored-By: Lucas <79713436+LucasVieiraS@users.noreply.github.com>

--- interface/blink.js
+++ interface/blink.js
@@ -4,7 +4,7 @@ const board = new five.Board({ port: 'COM3' });
 board.on("ready", () => {
   const led = new five.Led(6);
   led.pulse({
-    easing: "inOutSine",
+    easing: "inOutCube",
     duration: 5000,
     cuePoints: [0, 0.2, 0.4, 0.6, 0.8, 1],
     keyFrames: [0, 10, 0, 50, 0, 255],
