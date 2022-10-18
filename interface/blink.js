c8143ece83f646710f7f6c0f9d9faa8f6c828801
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-10-17 21:17:21 -0300

    new: range output; servo methods

--- interface/blink.js
+++ interface/blink.js
@@ -1,6 +1,21 @@
 const five = require("johnny-five");
 const board = new five.Board({ port: 'COM3' });
 
+let pinned = [];
+
+function moveServo(pin, degrees) {
+  let servoObject = null;
+  if (!pinned.includes(pin)) {
+    pinned[pin] = new five.Servo({
+      pin: pin,
+      range: [0, 180],
+      startAt: degrees
+    });
+  }
+  servoObject = pinned[pin]
+  servoObject.to(degrees);
+}
+
 board.on("ready", () => {
   const led = new five.Led(6);
   led.pulse({
